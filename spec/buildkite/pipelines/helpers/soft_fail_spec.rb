2ad68a6e764311d1b6e7a227050653f323ca7d76
Andrew W. Lee <git@drewlee.com>
2020-11-24 15:32:44 -0800

    Fix soft_fail behavior (#7)
    Fixes #5

--- spec/buildkite/pipelines/helpers/soft_fail_spec.rb
+++ spec/buildkite/pipelines/helpers/soft_fail_spec.rb
@@ -11,8 +11,44 @@ RSpec.describe Buildkite::Pipelines::Helpers::SoftFail do
 
   let(:step) { step_klass.new }
 
+  describe '#soft_fail' do
+    context 'with `true` as argument' do
+      let(:arg) { true }
+
+      it 'returns true' do
+        expect {
+          step.soft_fail(arg)
+        }.to change {
+          step.get(:soft_fail)
+        }.from(nil).to(true)
+      end
+
+      context 'with soft_fail previously set to an array' do
+        let(:arg) { [{ exit_status: 1 }, { exit_status: 2 }, { exit_status: 3 }] }
+
+        it 'raises when trying to be set to `true`' do
+          step.soft_fail(arg)
+          expect(step.soft_fail).to eq(arg)
+          expect { step.soft_fail(true) }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context 'with an array as argument' do
+      let(:arg) { [{ exit_status: 1 }, { exit_status: 2 }, { exit_status: 3 }] }
+
+      it 'returns an array of exit_statuses' do
+        expect {
+          step.soft_fail(arg)
+        }.to change {
+          step.get(:soft_fail)
+        }.from(nil).to(arg)
+      end
+    end
+  end
+
   describe '#soft_fail_on_status' do
-    it 'adds to soft falis' do
+    it 'adds to soft fails' do
       expect {
         step.soft_fail_on_status(1, 2, 3, 4, 5)
       }.to change {
