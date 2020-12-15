3fe3716984468127faeb8312922c53f0dce7ad35
Ngan Pham <ngan@users.noreply.github.com>
2020-12-15 13:44:35 -0800

    more steps

--- .buildkite/pipelines/showcase/pipeline.rb
+++ .buildkite/pipelines/showcase/pipeline.rb
@@ -7,4 +7,27 @@ Buildkite::Builder.pipeline do
     label "RSpec relabeled"
     command "echo 'do something else'"
   end
+
+  # Add complex conditions based on your cobebase as to whether or not a step
+  # should be defined.
+  if true == false
+    command do
+      label "This won't run"
+      command :noop
+    end
+  end
+
+  # Add a wait step.
+  wait
+
+  # Add a skipped step. You can see this step when you click the "eye" icon on
+  # the Buildkite web UI.
+  command do
+    label "Skipped Step"
+
+    # Conditionally skip a step.
+    if true == true
+      skip "This step is skipped because of x, y, and z"
+    end
+  end
 end
