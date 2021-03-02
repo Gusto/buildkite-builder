da533ba4979cc835856700e4b5e2139e6c9b74ef
Ngan Pham <ngan@users.noreply.github.com>
2021-03-02 07:31:09 -0800

    Use Forwardable

--- lib/buildkite/pipelines/steps/abstract.rb
+++ lib/buildkite/pipelines/steps/abstract.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+require "forwardable"
+
 module Buildkite
   module Pipelines
     module Steps
       class Abstract
+        extend Forwardable
         include Attributes
+
+        def_delegator :@context, :data
 
         attr_reader :pipeline
         attr_reader :template
-        attr_reader :data
 
         def self.to_sym
           name.split('::').last.downcase.to_sym
@@ -17,11 +21,10 @@ module Buildkite
         def initialize(pipeline, template = nil, **args, &block)
           @pipeline = pipeline
           @template = template
-          context = StepContext.new(self, **args)
-          @data = context.data
+          @context = StepContext.new(self, **args)
 
-          instance_exec(context, &template) if template
-          instance_exec(context, &block) if block_given?
+          instance_exec(@context, &template) if template
+          instance_exec(@context, &block) if block_given?
         end
       end
     end
