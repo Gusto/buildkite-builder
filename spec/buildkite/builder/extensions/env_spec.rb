27e354be4c22b5eff3bcdabd6040090855c41d6a
Xenor Chang <wei-fun.chang@gusto.com>
2021-08-16 16:17:27 +0800

    Add example spec for extension

---
+++ spec/buildkite/builder/extensions/env_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe Buildkite::Builder::Extensions::Env do
+  let(:context) do
+    Class.new do
+      def data
+        @data ||= Buildkite::Builder::Data.new
+      end
+    end.new
+  end
+  let(:dsl) do
+    Buildkite::Builder::Dsl.new(context).extend(described_class)
+  end
+
+  # Sets data
+  before { described_class.new(context) }
+
+  it 'adds `env` as dsl method to context and sets env data' do
+    expect {
+      dsl.env(FOO: '1')
+      dsl.env(BAR: '2')
+    }.not_to raise_error
+
+    expect(context.data.env).to eq('FOO' => '1', 'BAR' => '2' )
+  end
+
+  context 'when argument is not hash' do
+    it 'raises' do
+      expect {
+        dsl.env('FOO')
+      }.to raise_error(ArgumentError, 'value must be hash')
+    end
+  end
+end
