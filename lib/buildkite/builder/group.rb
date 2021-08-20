ba10412dd8ef76a62fece8a679550cd4fb3c4d27
Ngan Pham <ngan@users.noreply.github.com>
2021-08-20 10:16:36 -0700

    Pass along kwargs

--- lib/buildkite/builder/group.rb
+++ lib/buildkite/builder/group.rb
@@ -34,8 +34,8 @@ module Buildkite
         { group: label }.merge(attributes).merge(data.to_definition)
       end
 
-      def method_missing(method_name, *_args, &_block)
-        @dsl.public_send(method_name, *_args, &_block)
+      def method_missing(method_name, *args, **kwargs, &_block)
+        @dsl.public_send(method_name, *args, **kwargs, &_block)
       end
     end
   end
