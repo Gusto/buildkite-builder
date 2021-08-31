9bd489d72044483dc8ff70392db0d53d2d6a6f73
Xenor Chang <wei-fun.chang@gusto.com>
2021-08-31 11:23:42 +0800

    Tweak

--- lib/buildkite/builder/extensions/lib.rb
+++ lib/buildkite/builder/extensions/lib.rb
@@ -5,7 +5,7 @@ module Buildkite
         def prepare
           lib_dir = Buildkite::Builder.root.join(Buildkite::Builder::BUILDKITE_DIRECTORY_NAME, 'lib')
 
-          if lib_dir.exist? && lib_dir.directory? && !$LOAD_PATH.include?(lib_dir)
+          if lib_dir.directory? && !$LOAD_PATH.include?(lib_dir)
             $LOAD_PATH.unshift(lib_dir)
           end
         end
