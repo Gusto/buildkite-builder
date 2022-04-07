63dd02afddc6a5fbddc4670e49051014e46ddad4
Ngan Pham <ngan@users.noreply.github.com>
2022-04-06 19:12:29 -0700

    Remove specific repos in specs (#55)

--- spec/buildkite/pipelines/helpers/plugins_spec.rb
+++ spec/buildkite/pipelines/helpers/plugins_spec.rb
@@ -12,31 +12,25 @@ RSpec.describe Buildkite::Pipelines::Helpers::Plugins do
   let(:step) { step_klass.new(pipeline.data.steps, nil) }
 
   before do
-    pipeline.dsl.plugin(:code_cache, 'ssh://git@github.com/Gusto/code-cache-buildkite-plugin.git#65610a')
     pipeline.dsl.plugin(:docker_compose, 'docker-compose#v3.7.0')
     pipeline.dsl.plugin(:artifacts, 'artifacts#v1.3.0')
-    pipeline.dsl.plugin(:gusto_artifacts, 'ssh://git@github.com/Gusto/artifacts-buildkite-plugin.git#0.2')
+    pipeline.dsl.plugin(:foobar, 'ssh://git@github.com/example/foobar-buildkite-plugin.git#0.2')
     pipeline.dsl.plugin(:monorepo_diff, 'chronotc/monorepo-diff#v1.1.1')
     pipeline.dsl.plugin(:ecr, 'ecr#v2.0.0')
   end
 
   describe '#plugin' do
     it 'sets plugin' do
-      step.plugin(:code_cache)
-
-      expect(step.plugins.to_definition).to eq([{ 'ssh://git@github.com/Gusto/code-cache-buildkite-plugin.git#65610a' => nil }])
-
       step.plugin(:docker_compose)
       step.plugin(:artifacts)
-      step.plugin(:gusto_artifacts)
+      step.plugin(:foobar)
       step.plugin(:monorepo_diff)
       step.plugin(:ecr)
 
       expect(step.plugins.to_definition).to eq([
-        { 'ssh://git@github.com/Gusto/code-cache-buildkite-plugin.git#65610a' => nil },
         { 'docker-compose#v3.7.0' => nil },
         { 'artifacts#v1.3.0' => nil },
-        { 'ssh://git@github.com/Gusto/artifacts-buildkite-plugin.git#0.2' => nil },
+        { 'ssh://git@github.com/example/foobar-buildkite-plugin.git#0.2' => nil },
         { 'chronotc/monorepo-diff#v1.1.1' => nil },
         { 'ecr#v2.0.0' => nil },
       ])
