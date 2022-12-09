73162292f1356fe429ed85994671fbc3969f8378
Xenor Chang <tubaxenor@gmail.com>
2022-12-09 15:16:28 +0800

    Make sure group dsl extends all extensions

--- spec/buildkite/builder/group_spec.rb
+++ spec/buildkite/builder/group_spec.rb
@@ -2,18 +2,29 @@
 
 RSpec.describe Buildkite::Builder::Group do
   let(:root) { Buildkite::Builder.root }
-  let(:steps) { Buildkite::Builder::StepCollection.new(Buildkite::Builder::TemplateManager.new(root), Buildkite::Builder::PluginManager.new) }
+  let(:context) { OpenStruct.new(data: Buildkite::Builder::Data.new, root: Buildkite::Builder.root) }
+  let(:dsl) { Buildkite::Builder::Dsl.new(context) }
+
+  before do
+    context.data.steps = Buildkite::Builder::StepCollection.new(Buildkite::Builder::TemplateManager.new(root), Buildkite::Builder::PluginManager.new)
+    context.dsl = dsl
+  end
 
   describe '#to_h' do
-    let(:group) { described_class.new('foo', steps) }
+    let(:group) { described_class.new('foo', context) }
 
     it 'returns the data with group info' do
       expect(group.to_h).to eq(group: 'foo')
     end
 
     context 'with definitions' do
+      before do
+        dsl.extend(Buildkite::Builder::Extensions::Notify)
+        dsl.extend(Buildkite::Builder::Extensions::Steps)
+      end
+
       it 'returns the data with group step definitions' do
-        group = described_class.new('foo', steps) do
+        group = described_class.new('foo', context) do
           notify email: 'foo@bar.com'
           notify basecamp_campfire: 'basecamp_uri'
           command do
