89c52f2b85f859323c487a18038b7c15f1b331ad
Xenor Chang <tubaxenor@gmail.com>
2023-03-31 08:06:32 +0800

    Add signal_reason to automatic retry options

--- lib/buildkite/pipelines/helpers/retry.rb
+++ lib/buildkite/pipelines/helpers/retry.rb
@@ -4,15 +4,36 @@ module Buildkite
   module Pipelines
     module Helpers
       module Retry
-        def automatic_retry_on(exit_status:, limit:)
+        def automatic_retry_on(options = {})
+          raise 'limit must set for `automatic_retry_on`.' unless options[:limit]
+
+          if !options[:signal_reason] && !options[:exit_status]
+            raise 'signal_reason or exit_status must set for `automatic_retry_on`.'
+          end
+
           retry_value = get(:retry) || set(:retry, {})
 
           unless retry_value[:automatic].is_a?(Array)
             retry_value[:automatic] = []
           end
 
-          retry_value[:automatic].delete_if { |rule| rule[:exit_status] == exit_status }
-          retry_value[:automatic].push(exit_status: exit_status, limit: limit)
+          automatic_options = { limit: options[:limit] }
+
+          if options[:exit_status] && options[:signal_reason]
+            retry_value[:automatic].delete_if do |rule|
+              rule[:exit_status] == options[:exit_status] && rule[:signal_reason] == options[:signal_reason]
+            end
+            automatic_options[:exit_status] = options[:exit_status]
+            automatic_options[:signal_reason] = options[:signal_reason]
+          elsif options[:exit_status]
+            retry_value[:automatic].delete_if { |rule| rule[:exit_status] == options[:exit_status] }
+            automatic_options[:exit_status] = options[:exit_status]
+          elsif options[:signal_reason]
+            retry_value[:automatic].delete_if { |rule| rule[:signal_reason] == options[:signal_reason] }
+            automatic_options[:signal_reason] = options[:signal_reason]
+          end
+
+          retry_value[:automatic].push(automatic_options)
         end
 
         def automatic_retry(enabled)
--- spec/buildkite/pipelines/helpers/retry_spec.rb
+++ spec/buildkite/pipelines/helpers/retry_spec.rb
@@ -81,30 +81,130 @@ RSpec.describe Buildkite::Pipelines::Helpers::Retry do
   end
 
   describe '#automatic_retry_on' do
+    context 'when no limit' do
+      it 'raises' do
+        expect {
+          step.automatic_retry_on(exit_status: 254)
+        }.to raise_error('limit must set for `automatic_retry_on`.')
+      end
+    end
+
+    context 'when no exit_status and signal_reason' do
+      it 'raises' do
+        expect {
+          step.automatic_retry_on(limit: 1)
+        }.to raise_error('signal_reason or exit_status must set for `automatic_retry_on`.')
+      end
+    end
+
     context 'when retry was set' do
-      it 'pushes to the value array' do
-        step.retry(manual: false, automatic: [{ exit_status: 1, limit: 5 }])
+      context 'with exit_status' do
+        it 'pushes to the value array' do
+          step.retry(manual: false, automatic: [{ exit_status: 1, limit: 5 }])
 
-        step.automatic_retry_on(exit_status: 254, limit: 1)
+          step.automatic_retry_on(exit_status: 254, limit: 1)
 
-        expect(step.get(:retry)).to eq(
-          manual: false,
-          automatic: [
-            { exit_status: 1, limit: 5 },
-            { exit_status: 254, limit: 1 },
-          ]
-        )
+          expect(step.get(:retry)).to eq(
+            manual: false,
+            automatic: [
+              { exit_status: 1, limit: 5 },
+              { exit_status: 254, limit: 1 },
+            ]
+          )
+        end
+
+        it 'replaces same exit status' do
+          step.automatic_retry_on(exit_status: 123, limit: 1)
+          step.automatic_retry_on(exit_status: 123, limit: 2)
+
+          expect(step.get(:retry)).to eq(
+            automatic: [
+              { exit_status: 123, limit: 2 },
+            ]
+          )
+        end
       end
 
-      it 'replaces same exit status' do
-        step.automatic_retry_on(exit_status: 123, limit: 1)
-        step.automatic_retry_on(exit_status: 123, limit: 2)
+      context 'with signal_reason' do
+        it 'pushes to the value array' do
+          step.retry(manual: false, automatic: [{ signal_reason: 'none', limit: 5 }])
 
-        expect(step.get(:retry)).to eq(
-          automatic: [
-            { exit_status: 123, limit: 2 },
-          ]
-        )
+          step.automatic_retry_on(exit_status: 254, limit: 1)
+
+          expect(step.get(:retry)).to eq(
+            manual: false,
+            automatic: [
+              { signal_reason: 'none', limit: 5 },
+              { exit_status: 254, limit: 1 },
+            ]
+          )
+        end
+
+        it 'replaces same signal_reason' do
+          step.automatic_retry_on(signal_reason: '*', limit: 1)
+          step.automatic_retry_on(signal_reason: '*', limit: 2)
+
+          expect(step.get(:retry)).to eq(
+            automatic: [
+              { signal_reason: '*', limit: 2 },
+            ]
+          )
+        end
+      end
+
+      context 'with both signal_reason and exit_status' do
+        it 'pushes to the value array' do
+          step.retry(manual: false, automatic: [{ exit_status: 1, signal_reason: 'none', limit: 5 }])
+
+          step.automatic_retry_on(exit_status: 254, signal_reason: 'agent_stop', limit: 1)
+
+          expect(step.get(:retry)).to eq(
+            manual: false,
+            automatic: [
+              { exit_status: 1, signal_reason: 'none', limit: 5 },
+              { exit_status: 254, signal_reason: 'agent_stop', limit: 1 },
+            ]
+          )
+        end
+
+        it 'replaces same signal_reason and same exit_status' do
+          step.automatic_retry_on(signal_reason: '*', exit_status: 254, limit: 1)
+          step.automatic_retry_on(signal_reason: '*', exit_status: 254, limit: 2)
+
+          expect(step.get(:retry)).to eq(
+            automatic: [
+              { signal_reason: '*', exit_status: 254, limit: 2 },
+            ]
+          )
+        end
+
+        it 'does not replace when only one of the signal_reason or exit_status is different' do
+          step.automatic_retry_on(signal_reason: '*', exit_status: 254, limit: 1)
+          step.automatic_retry_on(signal_reason: '*', exit_status: 1, limit: 2)
+          step.automatic_retry_on(signal_reason: 'cancel', exit_status: 254, limit: 1)
+
+          expect(step.get(:retry)).to eq(
+            automatic: [
+              { signal_reason: '*', exit_status: 254, limit: 1 },
+              { signal_reason: '*', exit_status: 1, limit: 2 },
+              { signal_reason: 'cancel', exit_status: 254, limit: 1 },
+            ]
+          )
+        end
+
+        it 'replaces when only one of the signal_reason or exit_status provided' do
+          step.automatic_retry_on(signal_reason: '*', exit_status: 254, limit: 1)
+          step.automatic_retry_on(signal_reason: 'none', exit_status: 1, limit: 1)
+          step.automatic_retry_on(signal_reason: '*', limit: 2)
+          step.automatic_retry_on(exit_status: 1, limit: 1)
+
+          expect(step.get(:retry)).to eq(
+            automatic: [
+              { signal_reason: '*', limit: 2 },
+              { exit_status: 1, limit: 1 },
+            ]
+          )
+        end
       end
     end
 
