40695c6b23de8be1b3d236ec1b6dd17ab71d03f6
Xenor Chang <tubaxenor@gmail.com>
2023-04-11 00:47:23 +0800

    Add signal_reason to automatic retry options (#91)
    Co-authored-by: Josh Nichols <josh.nichols@gusto.com>

--- lib/buildkite/pipelines/helpers/retry.rb
+++ lib/buildkite/pipelines/helpers/retry.rb
@@ -4,15 +4,36 @@ module Buildkite
   module Pipelines
     module Helpers
       module Retry
-        def automatic_retry_on(exit_status:, limit:)
+        def automatic_retry_on(exit_status: nil, limit: nil, signal_reason: nil)
+          raise 'limit must set for `automatic_retry_on`.' unless limit
+
+          if exit_status.nil? && signal_reason.nil?
+            raise 'signal_reason or exit_status must set for `automatic_retry_on`.'
+          end
+
           retry_value = get(:retry) || set(:retry, {})
 
           unless retry_value[:automatic].is_a?(Array)
             retry_value[:automatic] = []
           end
 
-          retry_value[:automatic].delete_if { |rule| rule[:exit_status] == exit_status }
-          retry_value[:automatic].push(exit_status: exit_status, limit: limit)
+          automatic_options = { limit: limit }
+
+          if exit_status && signal_reason
+            retry_value[:automatic].delete_if do |rule|
+              rule[:exit_status] == exit_status && rule[:signal_reason] == signal_reason
+            end
+            automatic_options[:exit_status] = exit_status
+            automatic_options[:signal_reason] = signal_reason
+          elsif exit_status
+            retry_value[:automatic].delete_if { |rule| rule[:exit_status] == exit_status }
+            automatic_options[:exit_status] = exit_status
+          elsif signal_reason
+            retry_value[:automatic].delete_if { |rule| rule[:signal_reason] == signal_reason }
+            automatic_options[:signal_reason] = signal_reason
+          end
+
+          retry_value[:automatic].push(automatic_options)
         end
 
         def automatic_retry(enabled)
