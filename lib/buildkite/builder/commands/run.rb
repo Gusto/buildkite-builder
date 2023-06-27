40e1e7c809015c7c7b1f2d108631f97391f0a172
Xenor Chang <tubaxenor@gmail.com>
2023-06-27 15:42:22 +0800

    Switch back to use metedata.exists?

--- lib/buildkite/builder/commands/run.rb
+++ lib/buildkite/builder/commands/run.rb
@@ -17,7 +17,7 @@ module Buildkite
           # variables to be set. It also uploads the pipeline to Buildkite.
           log.info "+++ 🧰 #{'Buildkite Builder'.color(:springgreen)} v#{Buildkite::Builder.version} ─ #{relative_pipeline_path.to_s.yellow}"
 
-          if Buildkite::Pipelines::Command.meta_data(:get, Builder.meta_data.fetch(:job)) == Buildkite.env.step_id
+          if Buildkite::Pipelines::Command.meta_data(:exists, Builder.meta_data.fetch(:job))
             log.info "Pipeline already uploaded in #{Buildkite.env.step_id} step".color(:dimgray)
           else
             Pipeline.new(pipeline_path, logger: log).upload
--- spec/buildkite/builder/commands/run_spec.rb
+++ spec/buildkite/builder/commands/run_spec.rb
@@ -8,13 +8,15 @@ RSpec.describe Buildkite::Builder::Commands::Run do
   before do
     stub_const('ARGV', argv)
     setup_project(fixture_project)
+    stub_buildkite_env(step_id: 'step-id')
   end
 
   describe '.execute' do
-    context 'when step key matches' do
+    context 'when step key exists' do
+      let(:exists) { true }
+
       before do
-        stub_buildkite_env(step_id: 'step-id')
-        allow(Buildkite::Pipelines::Command).to receive(:meta_data).with(:get, Buildkite::Builder.meta_data.fetch(:job)).and_return('step-id')
+        allow(Buildkite::Pipelines::Command).to receive(:meta_data).with(:exists, Buildkite::Builder.meta_data.fetch(:job)).and_return(exists)
       end
 
       it 'does not upload the pipeline' do
@@ -23,24 +25,11 @@ RSpec.describe Buildkite::Builder::Commands::Run do
         described_class.execute
       end
 
-      context 'when uploaded to different job' do
+      context 'when step key does not exists' do
+        let(:exists) { false }
+
         before do
-          stub_buildkite_env(step_id: 'another-step-id')
-          allow(Buildkite::Pipelines::Command).to receive(:meta_data).with(:get, Buildkite::Builder.meta_data.fetch(:job)).and_return('step-id')
-        end
-
-        it 'uploads the context' do
-          expect(Buildkite::Builder::Pipeline).to receive(:new).and_return(pipeline)
-          expect(pipeline).to receive(:upload)
-
-          described_class.execute
-        end
-      end
-
-      context 'when not uploaded' do
-        before do
-          stub_buildkite_env(step_id: 'another-step-id')
-          allow(Buildkite::Pipelines::Command).to receive(:meta_data).with(:get, Buildkite::Builder.meta_data.fetch(:job)).and_return('')
+          allow(Buildkite::Pipelines::Command).to receive(:meta_data).with(:exists, Buildkite::Builder.meta_data.fetch(:job)).and_return(exists)
         end
 
         it 'uploads the context' do
