080ffc2bbfed6415af6984f74a8a2b4e0c4f7d21
Maple Ong <maple.develops@gmail.com>
2025-03-26 15:16:46 -0400

    Setup step for release (#127)

--- .buildkite/pipelines/buildkite-builder/pipeline.rb
+++ .buildkite/pipelines/buildkite-builder/pipeline.rb
@@ -3,6 +3,7 @@ Buildkite::Builder.pipeline do
   plugin :docker, "docker#v3.7.0"
 
   command do
+    key :rspec
     label emoji: :rspec
     command \
       "bundle",
@@ -18,5 +19,26 @@ Buildkite::Builder.pipeline do
       message: "${BUILDKITE_MESSAGE}",
       commit: "${BUILDKITE_COMMIT}",
       branch: "${BUILDKITE_BRANCH}"
+  end
+
+  if Buildkite.env.default_branch?
+    block do
+      key :confirm_publish
+      block ":rocket: Release to Docker Hub"
+      prompt "Push release to Docker Hub?"
+      depends_on :rspec
+    end
+
+    command do
+      label emoji: :docker
+      command "bundle", "rake docker:release"
+      plugin :docker,
+        image: "ruby:3.3"
+      plugin :docker,
+        build_args: {
+          version: File.read(File.expand_path("../../../../VERSION", __FILE__)).strip
+        }
+      depends_on :confirm_publish
+    end
   end
 end
